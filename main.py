503e012b4c4ed17681fc70a4ebb0e460bf7c2a84
Naman Shetty <64603506+Namanskshetty@users.noreply.github.com>
2021-06-10 11:14:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import subprocess
 subprocess.call("./bin.sh", shell=True)
 from core.base import Base
+from core.openGLUtlis import OpenGLUtlis
+from OpenGL import GL
+
+#render a single point
 
 class Test(Base):
   def initialize(self):
     print("starting..")
 
+
+    #create gpu pro
+
+    # vertex sader code
+    vsCode='''
+    void main()
+    {
+      gl_Position=vec4(0.0,0.0,0.0,1.0);
+    }'''
+
+    #fragment shader code  
+    fsCode='''
+    void main()
+    {
+      gl_FragColor=vec4(1.0,1.0,0.0,1.0);
+    }'''  
+    #send code to gpu to compile
+
+    self.programRef=OpenGLUtlis.initlizeProgram(vsCode,fsCode)
+
+    #render settings(optional) 
+    GL.glPointSize(16) 
   def update(self):
-    pass
+
+    # select program to use when rendering 
+    GL.glUseprogram(self.programRef)
+    #renders geometric object(s) using program
+    GL.glDrawArrays(GL.GL_POINTS,0,1)
 
 #create an instance of this class and run the program
 
