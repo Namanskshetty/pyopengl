8c3c6d86ea706c419d760b090d906bb0f2758680
Naman Shetty <64603506+Namanskshetty@users.noreply.github.com>
2021-06-11 11:08:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,42 +2,56 @@ import subprocess
 subprocess.call("./bin.sh", shell=True)
 from core.base import Base
 from core.openGLUtlis import OpenGLUtlis
-from OpenGL import GL
-
-#render a single point
+from OpenGL.GL import *
+from core.attribute import Attribute
+#render a 6 point
 
 class Test(Base):
   def initialize(self):
     print("starting..")
 
+    #create gpu program 
 
-    #create gpu pro
-
-    # vertex sader code
+    # vertex sader code(points)
     vsCode='''
+    in vec3 position;
     void main()
     {
-      gl_Position=vec4(0.0,0.0,0.0,1.0);
+      gl_Position=vec4(position.x,position.y,position.z,1.0);
     }'''
 
-    #fragment shader code  
+    #fragment shader code  (coor)
     fsCode='''
     void main()
     {
-      gl_FragColor=vec4(1.0,1.0,0.0,1.0);
+      gl_FragColor=vec4(0.0,1.0,1.0,1.0);
     }'''  
     #send code to gpu to compile
 
-    self.programRef=OpenGLUtlis.initlizeProgram(vsCode,fsCode)
+    self.programRef=OpenGLUtlis.initlizeProgram(vsCode, fsCode)
+    #render settings
+    glPointSize(16) 
+    glLineWidth(8)
 
-    #render settings(optional) 
-    GL.glPointSize(16) 
+    #set up VAOs(vertex array objects)
+    vaoRef= glGenVertexArrays(1)
+    glBindVertexArray(vaoRef)
+
+    #setup vertex Attribute
+    positionData=[[ 0.8,  0.0, 0.0],
+                  [ 0.4,  0.6, 0.0],
+                  [-0.4,  0.6, 0.0],
+                  [-0.8,  0.0, 0.0],
+                  [-0.4, -0.6, 0.0],
+                  [ 0.4, -0.6, 0.0]]
+    positionAttribute=Attribute("vec3",positionData)
+    positionAttribute.associateVariable(self.programRef,"position")
+    self.vertexCount=len(positionData)
   def update(self):
-
     # select program to use when rendering 
-    GL.glUseprogram(self.programRef)
+    glUseProgram(self.programRef)
     #renders geometric object(s) using program
-    GL.glDrawArrays(GL.GL_POINTS,0,1)
+    glDrawArrays(GL_TRIANGLE_FAN, 0, self.vertexCount)
 
 #create an instance of this class and run the program
 
