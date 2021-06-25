964d09afe6710dd4952afece029cfb1ac8e421a4
Naman Shetty <64603506+Namanskshetty@users.noreply.github.com>
2021-06-25 10:37:22 +0530

    Update uniform.py

--- core/uniform.py
+++ core/uniform.py
@@ -32,5 +32,13 @@ class Uniform(object):
             glUniform3f(self.variableRef,self.data[0],self.data[1],self.data[2])  
         elif self.dataType=="vec4":
             glUniform4f(self.variableRef,self.data[0],self.data[1],self.data[2],self.data[3])  
+        elif self.dataType=="mat4":
+            glUniformMatrix4fv(self.variableRef, 1, GL_TRUE, self.data)
         else:
             raise Exception("unkown uniform datatype "+self.dataType)
+
+
+
+
+
+                  
