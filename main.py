82d1b73660d973907ec0a8dfb927cb265f9cfd66
Naman Shetty <64603506+Namanskshetty@users.noreply.github.com>
2021-06-25 10:38:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from core.openGLUtlis import OpenGLUtlis
 from OpenGL.GL import *
 from core.attribute import Attribute
 from core.uniform import Uniform
-from math import sin
+from core.matrix import Matrix
+from math import pi
 
-#render multiple triangles
 class Test(Base):
   def initialize(self):
     print("starting..")
@@ -17,69 +17,62 @@ class Test(Base):
     # vertex sader code(points)
     vsCode='''
     in vec3 position;
-    uniform vec3 translation;
+    uniform mat4 projectionMatrix;
+    uniform mat4 modelMatrix;
     void main()
     {
-      vec3 pos = position + translation;
-      gl_Position=vec4(pos.x,pos.y,pos.z,1.0);
-    }'''
-
-    #fragment shader code 
-    fsCode='''
-    uniform vec3 baseColor;
+        gl_Position = projectionMatrix * modelMatrix * vec4(position, 1.0);
+    }
+    '''
+    fsCode="""
+    out vec4 fragColor;
     void main()
     {
-      gl_FragColor=vec4(baseColor.r,baseColor.g,baseColor.b,1.0);
-    }'''  
-    #send code to gpu to compile
+        fragColor=vec4(1.0, 0.0, 1.0,1.0);
+    }
+    """
 
-    self.programRef=OpenGLUtlis.initlizeProgram(vsCode, fsCode)
-    #vertx array object 
+    self.programRef=OpenGLUtlis.initlizeProgram(vsCode,fsCode)
 
-    #set up VAOs(vertex array objects)
-    vaoRef= glGenVertexArrays(1)
+    glClearColor(0.0,0.0,0.0,1.0)
+    glEnable(GL_DEPTH_TEST)
+
+    vaoRef=glGenVertexArrays(1)
     glBindVertexArray(vaoRef)
-
-    #setup vertex Attribute
     positionData=[[ 0.0,  0.2, 0.0],
-                  [ 0.2, -0.2, 0.0],
-                  [-0.2, -0.2, 0.0]]
+                  [ 0.1, -0.2, 0.0],
+                  [-0.1, -0.2, 0.0]]
     positionAttribute=Attribute("vec3",positionData)
     positionAttribute.associateVariable(self.programRef,"position")
     self.vertexCount=len(positionData)
-    #render settings
-    glClearColor(0, 0.1, 0.1, 1)
-    #set up uniforms
-    self.translation1=Uniform("vec3",[-0.5,0.0,0.0])
-    self.translation1.locateVariable(self.programRef,"translation")
-    self.translation2=Uniform("vec3",[0.5,0.0,0.0])#sets the diatance
-    self.translation2.locateVariable(self.programRef,"translation") 
+    mMatrix=Matrix.makeTranslation(0,0,-1)
+    self.modelMatrix=Uniform("mat4",mMatrix)
+    self.modelMatrix.locateVariable(self.programRef,"modelMatrix")
 
-    #to change color
-    self.baseColor1=Uniform("vec3",[1.0,0.0,0.0])
-    self.baseColor1.locateVariable(self.programRef,"baseColor")
-    self.baseColor2=Uniform("vec3",[0.0,0.0,1.0])
-    self.baseColor2.locateVariable(self.programRef,"baseColor")    
-    self.time=0
+    pMatrix=Matrix.makePerspective()
+    self.projectionMatrix=Uniform("mat4",pMatrix)
+    self.projectionMatrix.locateVariable(self.programRef,"projectionMatrix")
+
   def update(self):
-    # select program to use when rendering 
+    movAm=0.01
+    if self.input.isKeyPressed("w"):
+      m=Matrix.makeTranslation(0,movAm,0)
+      self.modelMatrix.data=m @ self.modelMatrix.data
+    if self.input.isKeyPressed("s"):
+      m=Matrix.makeTranslation(0,-movAm,0)
+      self.modelMatrix.data=m @ self.modelMatrix.data
+    if self.input.isKeyPressed("a"):
+      m=Matrix.makeTranslation(-movAm,0,0)
+      self.modelMatrix.data=m @ self.modelMatrix.data
+    if self.input.isKeyPressed("d"):
+      m=Matrix.makeTranslation(movAm,0,0)
+      self.modelMatrix.data=m @ self.modelMatrix.data
+
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glUseProgram(self.programRef)
-    glClear(GL_COLOR_BUFFER_BIT)#clear the screen
-    self.time+=1/60
-
-    #draw the first triangle
-    self.translation1.data[1]+=0.009
-    self.translation1.uploadData()
-    self.baseColor1.uploadData()
-    #renders geometric object(s) using program
-    glDrawArrays(GL_TRIANGLES, 0, self.vertexCount)
-    #draw the second triangle
-    self.baseColor2.data[2]=(sin(self.time)+1)/2
-    self.translation2.uploadData()
-    self.baseColor2.uploadData()
-    #renders geometric object(s) using program
-    glDrawArrays(GL_TRIANGLES, 0, self.vertexCount)
-
-#create an instance of this class and run the program
+    self.projectionMatrix.uploadData()
+    self.modelMatrix.uploadData()
+    glDrawArrays(GL_TRIANGLES,0,self.vertexCount)
 Test().run()
 # https://github.com/Namanskshetty/pyopengl
